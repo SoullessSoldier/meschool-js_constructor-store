e85cf95a7cc7901d1d3199a54a05c580f5779e62
Soulless Soldier <gevork_ssh@mail.ru>
2021-12-24 15:44:04 +0200

    add Extra day - timer on top + handle form sending

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,17 @@
 'use strict';
 
+const BACKEND_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 const buttonOrderItems = document.querySelectorAll('.product__button_order'),
     overlayOrder = document.querySelector('.overlay__order'),
-    modalOrder = document.querySelector('.modal__order');
+    modalOrder = document.querySelector('.modal__order'),
+    modalForm = document.querySelector('.modal__form'),
+    modalResult = document.querySelector('.modal__result');
 
 
 buttonOrderItems.forEach(buttonOrder => {
     buttonOrder.addEventListener('click', (e)=>{
-        const parentBlock = e.target.closest('.product__content');                
+        const parentBlock = e.target.closest('.product__content');
         overlayOrder.classList.add('overlay__active');
         modalOrder.value = parentBlock.querySelector('.product__title').textContent;    
     });
@@ -16,6 +20,49 @@ buttonOrderItems.forEach(buttonOrder => {
 overlayOrder.addEventListener('click', (e) => {
     const target = e.target;
     if (target.matches('.overlay__order') || target.closest('.modal__close')) {
+        modalResult.textContent = '';
         overlayOrder.classList.remove('overlay__active');
     }    
+});
+
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    new FormData(modalForm);    
+});
+
+modalForm.addEventListener('formdata', e => {    
+    let data = e.formData;
+    let result = '';
+
+    modalResult.textContent = '';
+
+    fetch(BACKEND_URL, {
+        method: 'POST',
+        headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+        body: JSON.stringify(Object.fromEntries(data.entries()))
+    })
+    .then(response => {
+        if(response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response.status);
+        }
+    })
+    .then(data => {
+        console.log(data);
+        result = 'Форма успешно отправлена';
+    })
+    .catch(err => {
+        console.warn(err);
+        result = 'Ошибка отправки формы';
+    })
+    .finally(()=>{
+            modalResult.textContent = result;
+            modalForm.reset();
+            //overlayOrder.classList.remove('overlay__active')
+        }        
+    );
 });
